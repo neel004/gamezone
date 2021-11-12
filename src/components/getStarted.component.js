220d7092dfb60b5f42901224c920e4eb7b6548d6
neel004 <neelp0004@gmail.com>
2021-11-12 15:05:35 +0530

    Added Get Started

--- src/components/getStarted.component.js
+++ src/components/getStarted.component.js
@@ -7,10 +7,10 @@ export default() => {
         <div className="home">
                 
                 <h3>How To Get Started?</h3>
-                <p><strong>Step 1</strong> : First Sign Up or if you already registed then Login in</p>
-                <p><strong>Step 2</strong> : After Successfull Login, user has to purchase minutes to Play Game</p>
-                <p><strong>Note</strong> : If user has windows then he should have install <a href="https://support.microsoft.com/en-us/windows/how-to-use-remote-desktop-5fe128d5-8fb1-7a23-3b8a-41e636865e8c">remote desktop connection</a>, for ubuntu <a href="https://ubuntu.com/tutorials/access-remote-desktop#1-overview">Remmina Remote Desktop</a> should be installed and for mac os install <a href="https://apps.apple.com/us/app/microsoft-remote-desktop/id1295203466?mt=12">Microsoft Remote Desktop</a> for playing games</p>
-                <p><strong>Step 3</strong> : After purchasing minutes Now user can play games</p>
+                <p><strong>Step 1</strong> : First Sign Up or if you already registed then Login in.</p>
+                <p><strong>Step 2</strong> : After Successfull Login, user has to purchase minutes to Play Game.</p>
+                <p><strong>Note</strong> : If user has windows then he should have install <a href="https://support.microsoft.com/en-us/windows/how-to-use-remote-desktop-5fe128d5-8fb1-7a23-3b8a-41e636865e8c">remote desktop connection</a>, for ubuntu <a href="https://ubuntu.com/tutorials/access-remote-desktop#1-overview">Remmina Remote Desktop</a> should be installed and for mac os install <a href="https://apps.apple.com/us/app/microsoft-remote-desktop/id1295203466?mt=12">Microsoft Remote Desktop</a> for playing games.</p>
+                <p><strong>Step 3</strong> : After purchasing minutes Now user can play games.</p>
         </div>
         );
     
